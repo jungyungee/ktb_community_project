c30f76bcee2db5a2fb6e2b19d371ab2bab92afe0
정윤지 <rosa.jung@rosa-jungs-MacBook-Pro.local>
2026-06-07 18:36:01 +0900

    [FEAT] post 엔티티 생성

---
+++ src/main/java/com/ktb/community/post/entity/Post.java
@@ -0,0 +1,64 @@
+package com.ktb.community.post.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import com.ktb.community.user.entity.User;
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@Entity
+@Table(name = "posts")
+public class Post {
+    //PK
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false, length = 26)
+    private String title;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+    @Column(name = "post_image_url")
+    private String postImageUrl;
+
+    @Column(name = "like_count")
+    private int likeCount = 0;
+
+    @Column(name = "comment_count")
+    private int commentCount = 0;
+
+    @Column(name = "view_count")
+    private int viewCount = 0;
+
+    @Column(name ="created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name ="updated_at")
+    private LocalDateTime updatedAt;
+
+    // 게시글 작성용 생성자 정의
+    public Post(User user, String title, String content, String postImageUrl) {
+        this.user = user;
+        this.title = title;
+        this.content = content;
+        this.postImageUrl = postImageUrl;
+    }
+
+    @PrePersist
+    public void onCreate(){
+        this.createdAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void onUpdate(){
+        this.updatedAt = LocalDateTime.now();
+    }
+}
