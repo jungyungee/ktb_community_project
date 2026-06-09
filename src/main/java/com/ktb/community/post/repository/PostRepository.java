f917e53943031ffdb65d8017a193f1609b16d987
정윤지 <rosa.jung@rosa-jungs-MacBook-Pro.local>
2026-06-09 13:32:13 +0900

    #3 [FEAT] 게시글 조회 페이징을 위한 커스텀 인터페이스 정의

--- src/main/java/com/ktb/community/post/repository/PostRepository.java
+++ src/main/java/com/ktb/community/post/repository/PostRepository.java
@@ -3,6 +3,8 @@ package com.ktb.community.post.repository;
 import com.ktb.community.post.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PostRepository extends JpaRepository<Post, Long> {
+import java.util.List;
+
+public interface PostRepository extends JpaRepository<Post, Long> , PostRepositoryCustom{
     // findById, save는 기본 제공
 }
