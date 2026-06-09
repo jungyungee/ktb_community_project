b2b59349e19d13d3c3586cfe3b8bc35581ba061c
정윤지 <rosa.jung@rosa-jungs-MacBook-Pro.local>
2026-06-09 14:16:35 +0900

    #3 [FEAT] PostRepositoryImpl 내에 findPostsbyCursor 구현

---
+++ src/main/java/com/ktb/community/post/repository/PostRepositoryImpl.java
@@ -0,0 +1,50 @@
+package com.ktb.community.post.repository;
+
+import com.ktb.community.post.entity.Post;
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+
+import static com.ktb.community.post.entity.QPost.post;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RequiredArgsConstructor
+// PostRepositoryCustom의 구현체 (Spring Data JPA Convention)
+public class PostRepositoryImpl implements PostRepositoryCustom{
+    private final JPAQueryFactory queryFactory;
+    // cursor (이전 마지막 게시글) 이 없으면 최신순으로 처음부터 조회
+    // cursor 있으면 그 다음부터 createdAt, id 두 조건을 이용해 조회
+    // createdAt DESC, id DESC 정렬
+    // 다음을 위해 (hasNext) 를 주기 위해 size+1 개 조회
+
+    @Override
+    public List<Post> findPostsByCursor(LocalDateTime createdAt, Long id, int size) {
+        // Where 절을 위한 객체 생성
+        BooleanBuilder condition = new BooleanBuilder();
+
+        // 첫 게시글이면 커서가 없음. 조건이 없음
+        // post의 createdAt 보다 작은 것 혹은 같다면 id가 작은 것부터
+        if (createdAt != null && id != null){
+            condition.and(
+                    post.createdAt.lt(createdAt)
+                            .or(
+                                    post.createdAt.eq(createdAt)
+                                            .and(post.id.lt(id))
+                            )
+            );
+        }
+
+        // post에서 위에 정의한 condition에 맞게 가져오고, 정렬해서 내보내기
+        return queryFactory
+                .selectFrom(post)
+                .where(condition)
+                .orderBy(
+                        post.createdAt.desc(),
+                        post.id.desc()
+                )
+                .limit(size + 1)
+                .fetch();
+    }
+}
