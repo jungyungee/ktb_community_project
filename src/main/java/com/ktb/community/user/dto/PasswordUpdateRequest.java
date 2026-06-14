4f590e1b6a28f751e23a2c6b338e9e9830755e3f
정윤지 <rosa.jung@rosa-jungs-MacBook-Pro.local>
2026-06-14 22:57:26 +0900

    #7 [ADD] 비밀번호 형식 패턴 추가

--- src/main/java/com/ktb/community/user/dto/PasswordUpdateRequest.java
+++ src/main/java/com/ktb/community/user/dto/PasswordUpdateRequest.java
@@ -1,10 +1,16 @@
 package com.ktb.community.user.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 
 @Getter
 public class PasswordUpdateRequest {
     @NotBlank
+    // 비밀번호 조건 패턴 추가
+    @Pattern(
+            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-=[\\]{};':\"\\\\|,.<>/?]).{8,20}$",
+            message = "password_format_invalid"
+    )
     private String newPassword;
 }
--- src/main/java/com/ktb/community/user/dto/SignupRequest.java
+++ src/main/java/com/ktb/community/user/dto/SignupRequest.java
@@ -2,6 +2,7 @@ package com.ktb.community.user.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 
 // 회원가입 요청 JSON을 담는 객체
@@ -11,6 +12,10 @@ public class SignupRequest {
     @Email
     private String email;
     @NotBlank
+    @Pattern(
+            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-=[\\]{};':\"\\\\|,.<>/?]).{8,20}$",
+            message = "password_format_invalid"
+    )
     private String password;
     @NotBlank
     private String nickname;
