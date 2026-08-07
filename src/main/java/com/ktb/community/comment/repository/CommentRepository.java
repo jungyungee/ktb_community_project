8baa2d73e82e6b9a4590d78b349897e102b283f8
정윤지 <rosa.jung@rosa-jungs-MacBook-Pro.local>
2026-08-07 14:22:11 +0900

    #12 [FEAT] 게시글 삭제 시 댓글 데이터 삭제 - bulk delete

--- src/main/java/com/ktb/community/comment/repository/CommentRepository.java
+++ src/main/java/com/ktb/community/comment/repository/CommentRepository.java
@@ -2,7 +2,15 @@ package com.ktb.community.comment.repository;
 
 import com.ktb.community.comment.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface CommentRepository extends JpaRepository<Comment, Long>, CommentRepositoryCustom{
-
+    @Modifying
+    @Query("""
+        DELETE FROM Comment c
+        WHERE c.post.id = :postId
+    """)
+    int deleteByPostId(@Param("postId") Long postId);
 }
