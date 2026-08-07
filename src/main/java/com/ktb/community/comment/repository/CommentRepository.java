abb2b224676114640dc0a04779b01c3d6818fe11
정윤지 <rosa.jung@rosa-jungs-MacBook-Pro.local>
2026-08-07 14:26:20 +0900

    #12 [FEAT] 게시글 삭제 시 연관된 좋아요 데이터 삭제 - bulk delete

--- src/main/java/com/ktb/community/comment/repository/CommentRepository.java
+++ src/main/java/com/ktb/community/comment/repository/CommentRepository.java
@@ -7,6 +7,8 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface CommentRepository extends JpaRepository<Comment, Long>, CommentRepositoryCustom{
+    // 게시글 삭제 -> 해당 게시글에 연관된 댓글 삭제
+    // @Query 사용한 JPQL Bulk delete
     @Modifying
     @Query("""
         DELETE FROM Comment c
